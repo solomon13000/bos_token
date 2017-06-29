817ecbfeb5a464b905cfb7cd009588c649a7d7b5
Eugene Anthony <solomon_13000@yahoo.com>
2017-06-29 21:31:08 +0800

    Update footer.php

--- public/assets/templates/theme/footer.php
+++ public/assets/templates/theme/footer.php
@@ -1,7 +1,7 @@
 		<footer class="navbar navbar-default navbar-fixed-bottom">
 		  <div class="container">
 			<div class="col-md-12 text-center">
-				<p>© 2017 - Powered by Relev Tech</p>
+				<p>© 2017 - Powered by</p>
 			</div>
 		  </div>
 		</footer>
